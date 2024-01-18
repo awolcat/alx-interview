474751dd1c4572e13e1231b0f592f040994bf043
Albert <albertirura11@gmail.com>
2024-01-18 21:51:53 +0300

    recursive GET

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,31 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-async function getCharacters (film) {
-  const url = 'https://swapi-api.alx-tools.com/api/films/' + film;
-  let characters = [];
-  request(url, (error, response, body) => {
+const filmId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + filmId;
+
+function getCharacter (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
     if (error) {
       console.log(error);
-    }
-    characters = JSON.parse(body).characters.slice();
-    for (const character of characters) {
-      request(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
+    } else {
+      console.log(JSON.parse(body).name);
+      getCharacter(characterList, index + 1);
     }
   });
 }
 
-const film = process.argv[2];
-getCharacters(film);
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    getCharacter(characterList, 0);
+  }
+});
