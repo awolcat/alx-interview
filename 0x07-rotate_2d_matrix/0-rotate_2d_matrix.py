64dd9c221808042d025fcdbca70450355590e856
Albert <albertirura11@gmail.com>
2024-01-27 15:01:50 +0300

    rotate 2-d square matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,4 +6,16 @@
 def rotate_2d_matrix(matrix):
     """Rotate 2-D matrix by 90 degrees in place
     """
-    print(matrix)
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+
+            top_left = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = top_left
+        right -= 1
+        left += 1
