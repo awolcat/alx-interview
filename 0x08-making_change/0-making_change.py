5c462ee3d28c19965bdd35ef9d7806c4870f3195
Albert <albertirura11@gmail.com>
2024-02-01 22:28:51 +0300

    quickie

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Making change
+"""
+
+def makeChange(coins, total):
+    """Fewest coins needed to make total
+    """
+    count = 0
+    index = 0
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)  # sort descending
+    while index < len(coins):
+        if coins[index] <= total and total > 0:
+            total -= coins[index]
+            count += 1
+        elif coins[index] > total:
+            index += 1
+        else:
+            break
+    if total != 0:
+        return -1
+    return count
