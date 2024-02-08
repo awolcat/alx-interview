476b2393902a278729f9f0e262481cb8606174bb
Albert <albertirura11@gmail.com>
2024-02-08 20:19:16 +0300

    perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -33,25 +33,16 @@ def count_zeros(grid, r, c):
         zeros += 1
     return zeros
 
+
 def island_perimeter(grid):
     """Find island perimeter
-    
-    grid is a list of list of integers:
-    0 represents water
-    1 represents land
-    Each cell is square, with a side length of 1
-    Cells are connected horizontally/vertically (not diagonally).
-    grid is rectangular, with its width and height not exceeding 100
-    The grid is completely surrounded by water
-    There is only one island (or nothing).
-    The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
     """
     perimeter = 0
     for r in range(len(grid)):
         for c in range(len(grid)):
             # check for 1,
-            # if 1, check up, down, left, right for 1 and count ones found. 
-            # zeros = 4 - ones found  
+            # if 1, check up, down, left, right for 1 and count ones found.
+            # zeros = 4 - ones found
             # sum all zeros found===perimeter
             if grid[r][c] == 1:
                 perimeter += count_zeros(grid, r, c)
